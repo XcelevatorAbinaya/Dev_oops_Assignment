b9012be14906f6cbafa513b0f3326b92ba36a74e
XcelevatorAbinaya <abinaya.2502@xcelevate.org>
2026-01-01 16:47:12 +0530

    Initial commit: CI pipeline with JaCoCo

---
+++ src/test/java/com/example/sample_java_assignment_app/SpringbootSampleAppApplicationIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.example.sample_java_assignment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SpringbootSampleAppApplicationIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testLoginEndpoint() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Please input your credentials for login..."));
+    }
+
+    @Test
+    void testRegisterEndpoint() throws Exception {
+        mockMvc.perform(get("/register"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Please follow registration process..."));
+    }
+
+
+}
